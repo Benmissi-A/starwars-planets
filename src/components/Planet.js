ec6ce0740a55c156676b5f394ca844d4e2f6f6a9
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-27 18:15:35 +0200

    starwars completed but no fonts

---
+++ src/components/Planet.js
@@ -0,0 +1,14 @@
+const Planet = ({planet}) => {
+  return <>
+    <div key = {planet.name} className="col-md-6  col-lg-4 col-xl-3 mb-4">
+      <article className="bg-warning p-3" >
+        <h2 className="h5">{planet.name}</h2>
+        <p className="mb-0"><b>population</b></p>
+        <p className="mb-0">{planet.population}</p>
+        <p className="mb-0"><b>climat</b></p>
+        <p className="mb-0">{planet.climate}</p>
+      </article>
+    </div>
+  </>
+}
+export default Planet
--- src/components/StarwarsPlanetApp.js
+++ src/components/StarwarsPlanetApp.js
@@ -1,10 +1,20 @@
-const { useEffect } = require("react")
+
+import { useEffect, useState } from "react"
+
+import Planet from "./Planet"
 
 const StarWarsPlanetApp = () => {
-  const [planet , setPlanet] = useState('')
+  const [planets , setPlanets] = useState([])
+  const [planetsCount,setPlanetsCount]= useState(0)
+  const [loading, setLoading] = useState(false)
+  const [apiCall , setApiCall] = useState(0)
+  const [error, setError] = useState('')
+  const [count, setCount] = useState(1)
   useEffect(() => {
-    fetch(`https://swapi.dev/api/planets`)
+    setLoading(true)
+    fetch(`https://swapi.dev/api/planets/?page=${count}`)
     .then((response) => {
+      setApiCall(a => a + 1)
       if(!response.ok){
         throw new Error(
           `something wrong ${response.status}`
@@ -13,10 +23,31 @@ const StarWarsPlanetApp = () => {
       return response.json()
     })
     .then((data) => {
-      console.log(data)
-      setPlanet(data)
+      setPlanets(planets => planets.concat(data.results))
+      setPlanetsCount(data.count)
     })
-  })
-  return <></>
+    .catch((error) => {
+      setError(error.message)
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }, [setApiCall,count,setPlanets,setPlanetsCount])
+  
+      console.log(count)
+  const handleClick = (e) => {
+    e.preventDefault()
+    setCount(count+1)
+  }
+  
+  return   <section className="container py-5">
+            <h1 className="mb-5">Planètes dans l'univer Star Wars</h1>
+            <div className="row">
+              { planets.map(el => <Planet key={el.name} planet={el}/> )}
+            </div>
+            {loading && <p>Loading....</p>}
+            {error && <p>{error}</p>} 
+          {planets.length === planetsCount ? <p class="bg-dark text-white p-3">Nous avons listé toutes les planètes recensées.</p> : <button type="button" className="btn btn-dark" onClick={handleClick}>Suivantes</button>}
+          </section>
 }
 export default StarWarsPlanetApp
